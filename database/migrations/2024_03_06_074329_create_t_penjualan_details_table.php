f71561e9e108269c493aa8d6b0325c00a47fd151
Zalbaq <ihza.nk.160903@gmail.com>
2024-03-06 16:08:25 +0700

    [create] table

---
+++ database/migrations/2024_03_06_074329_create_t_penjualan_details_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('t_penjualan_details', function (Blueprint $table) {
+            $table->id('detail_id');
+            $table->unsignedBigInteger('penjualan_id')->index();
+            $table->unsignedBigInteger('barang_id')->index();
+            $table->integer('harga');
+            $table->integer('harga_jumlah');
+
+            $table->foreign('barang_id')->references('barang_id')->on('m_barangs');
+            $table->foreign('penjualan_id')->references('penjualan_id')->on('t_penjualans');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('t_penjualan_details');
+    }
+};
---
+++ database/migrations/2024_03_06_074412_create_m_kategoris_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('m_kategoris', function (Blueprint $table) {
+            $table->id('kategori_id');
+            $table->string('kategori_kode', 10)->unique();
+            $table->string('kategori_nama', 100);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('m_kategoris');
+    }
+};
