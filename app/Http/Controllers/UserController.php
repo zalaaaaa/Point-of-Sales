86d1ffffa8e3bb134727edc1d32ffe4c057fe218
RaffyJO <rafiteguh6@gmail.com>
2024-03-08 16:03:58 +0700

    feat: retriev single model

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,25 +10,28 @@ class UserController extends Controller
 {
     public function index()
     {
-        // tambah data user dengan Eloquent Model
-        $data = [
-            'username' => 'Manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345'),
-            'level_id' => 2,
-        ];
-        UserModel::create($data); // tambah data ke tabel m_user
-
-        $user = UserModel::all(); // ambil semua data dari tabel m_user
-        return view('user', ['data' => $user]);
-
-        // $data = [
-        //     'nama' => 'Pelanggan Pertama',
-        // ];
-        // UserModel::where('username', 'customer-1')->update($data); // update data user
-
-        // // coba akses model UserModel
-        // $user = UserModel::all(); // ambil semua data dari tabel m_user
+        // // mencari user dengan primary key 1
+        // $user = UserModel::find(1);
         // return view('user', ['data' => $user]);
+
+        // // mencari user pertama dengan level id 1 menggunakan where
+        // $user = UserModel::where('level_id', 1)->first();
+        // return view('user', ['data' => $user]);
+
+        // // mencari user pertama dengan level id 1 menggunakan first where
+        // $user = UserModel::firstWhere('level_id', 1);
+        // return view('user', ['data' => $user]);
+
+        // // mencari menggunakan findOr
+        // $user = UserModel::findOr(1, ['username', 'nama'], function () {
+        //     abort(404);
+        // });
+        // return view('user', ['data' => $user]);
+
+        // mencari menggunakan findOr (tidak ditemukan)
+        $user = UserModel::findOr(20, ['username', 'nama'], function () {
+            abort(404);
+        });
+        return view('user', ['data' => $user]);
     }
 }
