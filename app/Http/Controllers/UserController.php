fb4c2bd75a2c60d84b3fd567e74cd14783422a30
RaffyJO <rafiteguh6@gmail.com>
2024-03-08 15:04:16 +0700

    feat: implement properti fillable

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,21 +11,24 @@ class UserController extends Controller
     public function index()
     {
         // tambah data user dengan Eloquent Model
-        // $data = [
-        //     'username' => 'customer-1',
-        //     'nama' => 'Pelanggan',
-        //     'password' => Hash::make('12345'),
-        //     'level_id' => 4,
-        // ];
-        // UserModel::insert($data); // tambah data ke tabel m_user
-
         $data = [
-            'nama' => 'Pelanggan Pertama',
+            'username' => 'Manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345'),
+            'level_id' => 2,
         ];
-        UserModel::where('username', 'customer-1')->update($data); // update data user
+        UserModel::create($data); // tambah data ke tabel m_user
 
-        // coba akses model UserModel
         $user = UserModel::all(); // ambil semua data dari tabel m_user
         return view('user', ['data' => $user]);
+
+        // $data = [
+        //     'nama' => 'Pelanggan Pertama',
+        // ];
+        // UserModel::where('username', 'customer-1')->update($data); // update data user
+
+        // // coba akses model UserModel
+        // $user = UserModel::all(); // ambil semua data dari tabel m_user
+        // return view('user', ['data' => $user]);
     }
 }
