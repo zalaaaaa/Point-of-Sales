a301a2e7c2d42becfc797529ed0f46115faaa079
RaffyJO <rafiteguh6@gmail.com>
2024-03-08 16:10:21 +0700

    feat: retriev single model with exceptions

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -28,10 +28,18 @@ class UserController extends Controller
         // });
         // return view('user', ['data' => $user]);
 
-        // mencari menggunakan findOr (tidak ditemukan)
-        $user = UserModel::findOr(20, ['username', 'nama'], function () {
-            abort(404);
-        });
+        // // mencari menggunakan findOr (tidak ditemukan)
+        // $user = UserModel::findOr(20, ['username', 'nama'], function () {
+        //     abort(404);
+        // });
+        // return view('user', ['data' => $user]);
+
+        // // mencari menggunakan findOrFail
+        // $user = UserModel::findOrFail(1);
+        // return view('user', ['data' => $user]);
+
+        // mencari menggunakan findOrFail (tidak ditemukan)
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
         return view('user', ['data' => $user]);
     }
 }
