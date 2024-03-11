7b3f2fb17eeed07a0a703f30fafc570875a99380
Zalbaq <ihza.nk.160903@gmail.com>
2024-03-11 23:20:04 +0700

    [add] timestamps column

--- database/migrations/2024_03_06_074412_create_m_kategoris_table.php
+++ database/migrations/2024_03_06_074412_create_m_kategoris_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id('kategori_id');
             $table->string('kategori_kode', 10)->unique();
             $table->string('kategori_nama', 100);
+            $table->timestamps();
         });
     }
 
