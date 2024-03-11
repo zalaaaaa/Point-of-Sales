13e299d01ac0b1a5136c31afec9844f88c8f6dae
Zalbaq <ihza.nk.160903@gmail.com>
2024-03-11 23:16:40 +0700

    [add] views for each controller

---
+++ resources/views/User.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Level Pengguna</title>
+</head>
+
+<body>
+    <h1>Data Level Pengguna</h1>
+    <table>
+        <tr>
+            <th>ID</th>
+            <th>Kode Level</th>
+            <th>Nama Level</th>
+        </tr>
+        @foreach ($data as $d)
+        <tr>
+            <td>{{$d->user_id}}</td>
+            <td>{{$d->username}}</td>
+            <td>{{$d->nama}}</td>
+            <td>{{$d->level_id}}</td>
+        </tr>
+        @endforeach
+    </table>
+</body>
+
+</html>
