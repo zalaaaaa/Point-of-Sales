69fdfac4e8fe7492a74885ce1ac8506b91339966
Zalbaq <ihza.nk.160903@gmail.com>
2024-03-16 05:40:41 +0700

    [push] Praktikum 1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,18 +14,14 @@ class UserController extends Controller
         // ->with('id', $id)
         // ->with('name', $name);
 
-        // $data = [
-        //     'username' => 'Customer 1',
-        //     'nama' => 'Pelanggan',
-        //     'password' => Hash::make('12345'),
-        //     'level_id' => 4
-        // ];
-        // UserModel::insert($data);
-
         $data = [
-            'nama' => 'Pelanggan Pertama'
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345')
         ];
-        UserModel::where('username', 'customer-1')->update($data);
+
+        UserModel::create($data);
 
         $user = UserModel::all();
         return view('user', ['data' => $user]);
