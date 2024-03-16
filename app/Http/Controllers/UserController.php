3d98a81066e1eeda1b217d33d7b8264e7c1d533a
Zalbaq <ihza.nk.160903@gmail.com>
2024-03-17 02:46:28 +0700

    [push] praktikum 2.5

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,41 +10,25 @@ class UserController extends Controller
 {
     public function index()
     {
-        // return view('user.profile')
-        // ->with('id', $id)
-        // ->with('name', $name);
-
-        // $data = [
-        //     'level_id' => 2,
-        //     'username' => 'manager_tiga',
-        //     'nama' => 'Manager 3',
-        //     'password' => Hash::make('12345')
-        // ];
-
-        // UserModel::create($data);
-
-        // $user = UserModel::all();
-        // return view('user', ['data' => $user]);
-
-        // $user = UserModel::firstWhere('level_id', 1);
-        // $user = UserModel::findOr(20, ['username', 'nama'], function () {
-        //     abort(404);
-        // });
-
-        // $user = UserModel::findOrFail(1);
-        // $user = UserModel::where('username', 'manager9')->firstOrFail();
-
-        // $user = UserModel::where('level_id', 2)->count();
-
         $user = UserModel::firstOrNew(
             [
-                'username' => 'manager33',
-                'nama' => 'Manager Tiga Tiga',
+                'username' => 'manager11',
+                'nama' => 'Manager11',
                 'password' => Hash::make('12345'),
                 'level_id' => 2
             ]
         );
+
+        $user->username = 'manager12';
+
         $user->save();
-        return view('user', ['data' => $user]);
+
+        $user->wasChanged();
+        $user->wasChanged('username');
+        $user->wasChanged(['username', 'level_id']);
+        $user->wasChanged('nama');
+
+        dd($user->wasChanged(['nama', 'username']));
+        // return view('user', ['data' => $user]);
     }
 }
