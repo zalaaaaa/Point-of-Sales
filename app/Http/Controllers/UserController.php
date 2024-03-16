716bc144ce00c630bf194b40564c4ad9fe953c47
Zalbaq <ihza.nk.160903@gmail.com>
2024-03-16 08:17:12 +0700

    [push] Praktikum 2.4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -34,7 +34,17 @@ class UserController extends Controller
         // $user = UserModel::findOrFail(1);
         // $user = UserModel::where('username', 'manager9')->firstOrFail();
 
-        $user = UserModel::where('level_id', 2)->count();
+        // $user = UserModel::where('level_id', 2)->count();
+
+        $user = UserModel::firstOrNew(
+            [
+                'username' => 'manager33',
+                'nama' => 'Manager Tiga Tiga',
+                'password' => Hash::make('12345'),
+                'level_id' => 2
+            ]
+        );
+        $user->save();
         return view('user', ['data' => $user]);
     }
 }
